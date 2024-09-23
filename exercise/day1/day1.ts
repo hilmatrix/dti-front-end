efb7f60b5c286e9880f76053def0061bce020b67
Hilmatrix <hilmaul@gmail.com>
2024-09-23 16:55:39 +0700

    finish all task exercise 1

--- exercise/day1/day1.ts
+++ exercise/day1/day1.ts
@@ -1,9 +1,42 @@
 export function day1() {
     task1();
+    task2();
+    task3();
+    task4();
+    task5();
 }
 
 function task1() {
-    let length = 5;
-    let width = 3;
-    console.log("Rectangle Area = " + length * width);
+    const length = 5;
+    const width = 3;
+    console.log(`Rectangle Area = ${length * width}`);
+}
+
+function task2() {
+    const diameter = 5;
+    console.log("Circle Circumference = " + 2*Math.PI*diameter);
+    console.log(`Circle Area = ${Math.PI * diameter**2}`);
+}
+
+function task3() {
+    const angleA = 80;
+    const angleB = 65;
+    console.log(`Angle C = ${(angleA - angleB)}`);
+}
+
+function task4() {
+    const date1 = "2024-03-19";
+    const date2 = "2024-03-21";
+    const startDate = new Date(date1);
+    const endDate = new Date(date2);
+    const timeDifference = endDate.getTime() - startDate.getTime();
+    const dayDifference = timeDifference / (1000 * 60 * 60 * 24);
+    console.log(`Days difference = ${dayDifference}`);
+}
+
+function task5() {
+    const fullName = "Hilman Mauludin";
+    const words = fullName.split(" ");
+    const initials = words.map(word => word[0]).join("");
+    console.log(`Initials = ${initials}`); 
 }
